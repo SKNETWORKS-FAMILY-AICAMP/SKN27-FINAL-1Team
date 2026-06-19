a30c935a7eb0ad7585c77d980ead625a83022ae1
wynn3312 <104gojib2@naver.com>
2026-06-19 17:27:30 +0900

    [feature_wynn3312][김경수] 조리방법 크롤링 용 파일 추가

---
+++ etl/recipe/preprocessing/cooking_step_crawling.py
@@ -0,0 +1,29 @@
+import pathlib
+import pandas as pd
+import requests
+import beautifulsoup4 as bs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################### 실행 함수 ############################
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
